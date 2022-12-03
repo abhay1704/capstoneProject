f25d25f7e1e43db08fe0b74b4e9c34c5741a85e6
Abhay <abhaypratapninth@gmail.com>
2022-12-03 14:42:35 +0530

    Added Innovation page and some contents in home page

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ function openNav() {
     var nav = document.querySelector(".nav");
     nav.classList.toggle("onMoreClick");
     setTimeout( () => { nav.classList.remove("onMoreClick") },1000);
-
 }
